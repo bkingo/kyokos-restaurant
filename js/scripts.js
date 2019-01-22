fa0b0d74434843f9d77b6c01553af48b989dadd9
Brandon King <brandon@brandonking.io>
2019-01-22 00:39:53 -0500

    completed basic functionality for menu selector

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,22 @@ const menuSections = document.querySelectorAll('.menu__section');
 // add function to each menu tab such that clicking the element, it will open that corresponding section and close the others
 tabList.forEach(function(tab) {
     tab.addEventListener('click', function(e) {
-        
-        selectedTab = tab.textContent.toLowerCase();
+        // set color of selected tab to black and others to gray
+        tabList.forEach(function(tab) {
+            if (tab !== e.target) {
+                tab.style.color = '#9B9E95';
+            } else {
+                tab.style.color = '#3d373f';
+            }
+        });
+
+        // search for menu section corresponding to selected tab and close the others
         menuSections.forEach(function(menuSection) {
-            if (menuSection.dataset.menu__section === selectedTab) {
+            if (menuSection.dataset.menu__section === e.target.textContent.toLowerCase()) {
                 menuSection.style.display = 'block';
             } else {
                 menuSection.style.display = 'none';
             }
-            
         });
 
         
